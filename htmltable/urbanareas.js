36601dd8b8654889118b3958f8d1a62fc428be2d
sarahfollowill <92276474+sarahfollowill@users.noreply.github.com>
2022-08-24 21:55:01 -0400

    hw_htmltable

---
+++ htmltable/urbanareas.js
@@ -0,0 +1,42 @@
+var urbanareas = {
+   "objects": [ 
+    {
+        "OBJECTID": "1",
+        "Year": "1950",
+        "Square_Miles": "110",
+        "Acres": "70343",
+        "GlobalID": "{4DF873DA-E907-4C0F-B74D-5604332C4052}"
+    },
+    {
+        "OBJECTID": "2",
+        "Year": "1970",
+        "Square_Miles": "490",
+        "Acres": "313715",
+        "GlobalID": "{62CD6120-F1B3-46E3-BF04-979CC54E4AD8}"
+    },
+    {
+        "OBJECTID": "3",
+        "Year": "1990",
+        "Square_Miles": "1900",
+        "Acres": "1215948",
+        "GlobalID": "{2A497177-103F-4761-BD18-2E4F217819D9}"
+    },
+    {
+        "OBJECTID": "4",
+        "Year": "2000",
+        "Square_Miles": "3770",
+        "Acres": "2412878",
+        "GlobalID": "{D5DFA50C-D7D1-4F63-B8E1-50F45486535F}"
+    },
+    {
+        "OBJECTID": "5",
+        "Year": "2010",
+        "Square_Miles": "5268",
+        "Acres": "3371295",
+        "GlobalID": "{B535DFB6-6557-4F97-87B0-B591728A496D}"
+    }
+    ],
+    "Year": [1950, 1970, 1990, 2000, 2010],
+    "Square_Miles": [110, 490, 1900, 3770, 5268],
+    "Acres": [70343, 313715, 1215948, 2412878, 3371295]
+}
